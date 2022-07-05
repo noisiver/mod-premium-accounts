c1fe2868b1c86444aae1f4364bad6d1f75929023
Revision <tkn963@gmail.com>
2022-07-05 19:32:56 +0200

    Missed
    I missed these

--- data/sql/db-world/base/premium_accounts.sql
+++ data/sql/db-world/base/premium_accounts.sql
@@ -1,4 +1,4 @@
-DELETE FROM `command` WHERE `name` IN ('premium account add', 'premium account remove', 'premium account addbit', 'premium account removebit');
+DELETE FROM `command` WHERE `name` IN ('premium account add', 'premium account remove', 'premium account additem', 'premium account removeitem');
 INSERT INTO `command` (`name`, `security`, `help`) VALUES 
 ('premium account add', 2, 'Syntax: .premium account add [$account]\r\n\r\nEnable premium features for the account'),
 ('premium account remove', 2, 'Syntax: .premium account remove [$account]\r\n\r\nDisable premium features for the account'),
